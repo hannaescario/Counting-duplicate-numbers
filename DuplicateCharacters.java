0cd7376ad112667dcd59ef8361f0e5496d6ebe90
hannaescario <100050898+hannaescario@users.noreply.github.com>
2022-02-21 11:54:54 +0800

    Add files via upload

--- DuplicateCharacters.java
+++ DuplicateCharacters.java
@@ -4,6 +4,18 @@ public class DuplicateCharacters {
 
   public static void main(String[] args) {
 
+    // given input string
+    String input = "JuoginJanhelEE";
+
+   // convert string into stream
+   Map<Character, Long> result = input.chars() .mapToObj(c -> (char) c) 
+   .collect(Collectors.groupingby(c -> c, Collectors.counting()));
+   
+   result.forEach((k, v) -> {
+     if(v >  1) {
+        System.out.println(k + " : " + v);
+     }
+   });
   }
 
 }
