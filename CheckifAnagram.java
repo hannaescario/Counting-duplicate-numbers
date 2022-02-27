2c7e820bdb249d11adfdff51ee4c5f69b423f136
hannaescario <100050898+hannaescario@users.noreply.github.com>
2022-02-27 10:27:18 +0800

    Add files via upload

---
+++ CheckifAnagram.java
@@ -0,0 +1,33 @@
+package ComProg2;
+
+import java.sql.Array;
+
+
+public class CheckifAnagram {
+
+  public static void main(String[] args) {
+  
+  String str1 = "abcde";
+  String str2 = "ecdab";
+  
+  if(str1.length()!=str2.lenght())
+  {
+    System.out.println("length is different then the strings are not anagram");
+  }
+  }
+ else {
+       
+       char[] ch1 = str1.toCharArray();
+       char[] ch2 = str2.toCharArray();
+       
+       Array.sort(ch1);
+       Array.sort(ch2);
+       
+       if (Arrays.equals(ch1,ch2)){
+         System.out.println("Both strings are anagram");
+       }
+       else {
+         System.out.println("Both strings are not anagram");
+       }
+ }
+}
